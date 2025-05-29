5cde2602c40d44e6887652591dfb6fc40314db39
Olivia Perry <153868993+4orks@users.noreply.github.com>
2025-05-29 16:48:34 +0100

    Add custom form validation
    This commit adds custom javascript form validation methods to the new
    book form.

--- scripts.js
+++ scripts.js
@@ -8,7 +8,7 @@ class Book {
         this.pages = pages;
         this.read = read;
     }
-};
+}
 
 Book.prototype.toggleRead = function () {
     this.read = !this.read;
@@ -17,14 +17,14 @@ Book.prototype.toggleRead = function () {
 function addBookToLibrary(title, author, pages, read) {
     const book = new Book(title, author, pages, read);
     myLibrary.push(book);
-};
+}
 
 function createTable() {
     var headers = ["Title", "Author", "Pages", "Read?", "ID", "Delete"];
     var table = document.createElement("table");
     table.setAttribute("id", "bookTable");
 
-    for(i=0; i<myLibrary.length; i++) {
+    for (i = 0; i < myLibrary.length; i++) {
         const book = myLibrary[i];
         var row = table.insertRow(i);
         row.insertCell(0).innerHTML = myLibrary[i].title;
@@ -36,7 +36,9 @@ function createTable() {
         readBtn.innerText = book.read ? "Read" : "Unread";
         readBtn.setAttribute("data-id", book.id);
         readBtn.addEventListener("click", function () {
-            const bookToToggle = myLibrary.find(b => b.id === this.getAttribute("data-id"));
+            const bookToToggle = myLibrary.find(
+                (b) => b.id === this.getAttribute("data-id")
+            );
             if (bookToToggle) {
                 bookToToggle.toggleRead();
                 document.querySelector("table").remove();
@@ -59,55 +61,87 @@ function createTable() {
 
     var header = table.createTHead();
     var headerRow = header.insertRow(0);
-    for(i=0; i<headers.length; i++) {
+    for (i = 0; i < headers.length; i++) {
         headerRow.insertCell(i).innerHTML = headers[i];
     }
 
     document.body.insertBefore(table, document.body.childNodes[2]);
-};
+}
 
 function deleteBookById(id) {
-    const index = myLibrary.findIndex(book => book.id === id);
+    const index = myLibrary.findIndex((book) => book.id === id);
     if (index !== -1) {
         myLibrary.splice(index, 1);
         document.querySelector("table").remove();
         createTable();
     }
-};
-
-function toggleReadStatus(id) {
-    
 }
+
+function toggleReadStatus(id) {}
 
 createTable();
 
 function handleNewButtonClick() {
     const newBookForm = document.createElement("form");
     newBookForm.innerHTML = `
-    <h2>New Book</h2>
-    <div id="titleDiv">
-      <label for="title">Title</label>
-      <input id="title" name="title">
-    </div>
-    <div id="authorDiv">
-      <label for="author">Author</label>
-      <input id="author" name="author">
-    </div>
-    <div id="pagesDiv">
-      <label for="pages">Number of Pages</label>
-      <input id="pages" name="pages" type="number">
-    </div>
-    <div id="readDiv">
-      <label for="read">Have you read this book?</label>
-      <input id="read" name="read" type="checkbox">
-    </div>
-    <button type="button" id="submit">Add Book</button>
-  `;
-  document.body.appendChild(newBookForm);
-  document.querySelector("#newBook").remove();
+        <h2>New Book</h2>
+        <div id="titleDiv">
+            <label for="title">Title</label>
+            <input id="title" name="title" required>
+        </div>
+        <div id="authorDiv">
+            <label for="author">Author</label>
+            <input id="author" name="author" required>
+        </div>
+        <div id="pagesDiv">
+            <label for="pages">Number of Pages</label>
+            <input id="pages" name="pages" type="number" required>
+        </div>
+        <div id="readDiv">
+            <label for="read">Have you read this book?</label>
+            <input id="read" name="read" type="checkbox">
+        </div>
+        <button type="button" id="submit">Add Book</button>
+    `;
+    document.body.appendChild(newBookForm);
+    document.querySelector("#newBook").remove();
+
+    const title = document.querySelector("#title");
+    title.addEventListener("input", () => {
+        if (title.validity.valueMissing) {
+            title.setCustomValidity("Required field");
+        } else {
+            title.setCustomValidity("");
+        }
+    });
+    const author = document.querySelector("#author");
+    author.addEventListener("input", () => {
+        if (author.validity.valueMissing) {
+            author.setCustomValidity("Required field");
+        } else {
+            author.setCustomValidity("");
+        }
+    });
+    const pages = document.querySelector("#pages");
+    pages.addEventListener("input", () => {
+        if (pages.validity.valueMissing) {
+            pages.setCustomValidity("Required field");
+        } else if (pages.validity.badInput || pages.valueAsNumber <= 0) {
+            pages.setCustomValidity("Must be a positive number")
+        } else {
+            pages.setCustomValidity("");
+        }
+    });
 
     const submit = document.querySelector("#submit");
-    submit.addEventListener("click", function() {
+    submit.addEventListener("click", function () {
+        const form = document.querySelector("form");
+        
+        if (!form.checkValidity()) {
+            form.reportValidity();
+            return;
+        }
+
         addBookToLibrary(
             document.querySelector("#title").value,
             document.querySelector("#author").value,
@@ -116,14 +150,14 @@ function handleNewButtonClick() {
         );
         document.querySelector("form").remove();
         document.querySelector("table").remove();
-        const newBook = document.createElement("button")
+        const newBook = document.createElement("button");
         newBook.setAttribute("id", "newBook");
         newBook.innerHTML = "New Book";
         createTable();
         document.body.appendChild(newBook);
         newBook.addEventListener("click", handleNewButtonClick);
     });
-};
+}
 
 const newBook = document.querySelector("#newBook");
 newBook.addEventListener("click", handleNewButtonClick);
